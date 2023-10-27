bcf3c1bdeb37b7f34dba4741a2e0af5f548b7a79
atamayo <atamayoquintero@gmail.com>
2023-10-27 17:41:59 -0500

    feat: add icon to extension

--- dist/assets/index-48446886.js
+++ dist/assets/index-48446886.js
@@ -7584,6 +7584,59 @@ var m = reactDomExports;
   client.createRoot = m.createRoot;
   client.hydrateRoot = m.hydrateRoot;
 }
+const TabIndexHighlighter = () => {
+  const [showTabIndexes, setShowTabIndexes] = reactExports.useState(false);
+  const codeToExecute = function(showTabIndexes2) {
+    if (showTabIndexes2) {
+      const indicators = document.querySelectorAll(".tabindex-indicator");
+      indicators.forEach((indicator) => indicator.remove());
+    } else {
+      const elements = document.querySelectorAll("[tabindex]");
+      elements.forEach((element) => {
+        if (element instanceof HTMLElement) {
+          const valueTabIndex = element.getAttribute("tabindex") || "unknown";
+          const positionStyle = window.getComputedStyle(element).position;
+          if (positionStyle === "static") {
+            element.style.position = "relative";
+          }
+          const span = document.createElement("span");
+          span.className = "tabindex-indicator";
+          span.style.position = "absolute";
+          span.style.padding = "2px";
+          span.style.top = "0";
+          span.style.left = "0";
+          span.style.zIndex = "999";
+          span.style.background = "red";
+          span.style.color = "white";
+          span.style.fontSize = "11px";
+          span.innerText = valueTabIndex;
+          const numericValueTabIndex = parseInt(valueTabIndex, 10);
+          if (numericValueTabIndex > 1) {
+            span.innerText += "⚠️";
+          }
+          element.appendChild(span);
+        }
+      });
+    }
+  };
+  const handleClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      if (currentTab && currentTab.id) {
+        const tabId = currentTab.id;
+        chrome.scripting.executeScript(
+          {
+            target: { tabId },
+            func: codeToExecute,
+            args: [showTabIndexes]
+          },
+          () => setShowTabIndexes(!showTabIndexes)
+        );
+      }
+    });
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("button", { onClick: handleClick, children: showTabIndexes ? "Hide TI" : "Show TI" });
+};
 const AutocompleteHighlighter = () => {
   const [showAutoCompletes, setShowAutoCompletes] = reactExports.useState(false);
   const codeToExecute = function(showAutoCompletes2) {
@@ -7737,59 +7790,6 @@ const AutocompleteHighlighter = () => {
     });
   };
   return /* @__PURE__ */ jsxRuntimeExports.jsx("button", { onClick: handleClick, children: showAutoCompletes ? "Hide AC" : "Show AC" });
-};
-const TabIndexHighlighter = () => {
-  const [showTabIndexes, setShowTabIndexes] = reactExports.useState(false);
-  const codeToExecute = function(showTabIndexes2) {
-    if (showTabIndexes2) {
-      const indicators = document.querySelectorAll(".tabindex-indicator");
-      indicators.forEach((indicator) => indicator.remove());
-    } else {
-      const elements = document.querySelectorAll("[tabindex]");
-      elements.forEach((element) => {
-        if (element instanceof HTMLElement) {
-          const valueTabIndex = element.getAttribute("tabindex") || "unknown";
-          const positionStyle = window.getComputedStyle(element).position;
-          if (positionStyle === "static") {
-            element.style.position = "relative";
-          }
-          const span = document.createElement("span");
-          span.className = "tabindex-indicator";
-          span.style.position = "absolute";
-          span.style.padding = "2px";
-          span.style.top = "0";
-          span.style.left = "0";
-          span.style.zIndex = "999";
-          span.style.background = "red";
-          span.style.color = "white";
-          span.style.fontSize = "11px";
-          span.innerText = valueTabIndex;
-          const numericValueTabIndex = parseInt(valueTabIndex, 10);
-          if (numericValueTabIndex > 1) {
-            span.innerText += "⚠️";
-          }
-          element.appendChild(span);
-        }
-      });
-    }
-  };
-  const handleClick = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentTab = tabs[0];
-      if (currentTab && currentTab.id) {
-        const tabId = currentTab.id;
-        chrome.scripting.executeScript(
-          {
-            target: { tabId },
-            func: codeToExecute,
-            args: [showTabIndexes]
-          },
-          () => setShowTabIndexes(!showTabIndexes)
-        );
-      }
-    });
-  };
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("button", { onClick: handleClick, children: showTabIndexes ? "Hide TI" : "Show TI" });
 };
 const AriaRolesHighlighter = () => {
   const [showAriaRoles, setShowAriaRoles] = reactExports.useState(false);
