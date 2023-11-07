7cb8d089ba1ee3c14203141fd8fa1a5ab48cb3ce
atamayo <atamayoquintero@gmail.com>
2023-11-07 08:37:06 -0500

    fix: update button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,6 +9,8 @@ export const CustomButton = (props: ButtonProps): JSX.Element => {
       className={props.className}
       disabled={props.disabled}
       onClick={props.onClick}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
     >
       {props.children}
     </button>
